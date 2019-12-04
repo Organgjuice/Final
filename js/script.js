675e6b294646234096cf64ef7effecbcfc07eb0e
Julia Bowman <julia@Julias-MacBook-Pro.local>
2019-12-03 21:26:54 -0600

    update

--- js/script.js
+++ js/script.js
@@ -12,69 +12,51 @@ var multiply ={
       x5: [316,360]
   }
 }
-var bank = 1000000
-var bankText = $('bankText')
-bankText.innerText = bank
+var bank = 60000
+var bankText = $('#bankText')
+bankText.text(bank)
 
 
  var wheel = $("#wheel")
  var button  = $("#spin-btn")
-button.addEventListener ("click", function(e){
+button.click(function(e){
   var rando =Math.floor(Math.random() *360)
-    wheel.style.transform = 'rotate('+rando+'deg)'
-  console.log(rando)
+    wheel.css('transform' , 'rotate('+rando+'deg)')
+
   if (rando > 0 && rando <=45){
     console.log('Add x2')
-    bank = bank + (20000 * 2)
-    console.log(bank)
-   bankText.innerText =bank
+  bank = bank + (20000 * 2)
+  bankText.text(bank)
  
   }
   else if ( rando >45 && rando <=90){
-    console.log('Subtract x2')
-    bank = bank -(20000 * 2)
-    console.log(bank)
-    bankText.innerText =bank
+   bank = bank -(20000 * 2)
+   bankText.text(bank)
   }
   else if (rando > 90 && rando <=135){
-    console.log('Add x3')
-    bank = bank + (20000 * 3)
-    console.log(bank)
-    bankText.innerText =bank
+  bank = bank + (20000 * 3)
+  bankText.text(bank)
   }
   else if(rando >135 && rando <=180){
-    console.log("Subtract x3")
-    bank = bank -(20000 * 3)
-    console.log(bank)
-    bankText.innerText =bank
+  bank = bank -(20000 * 3)
+  bankText.text(bank)
   }
   else if(rando >180 && rando <=225){
-    console.log('Add x4')
-    bank = bank + (20000 + 80000)
-    console.log(bank)
-    bankText.innerText =bank
+  bank = bank + (20000 + 80000)
+  bankText.text(bank)
   }
-    else if(rando >225 && rando <=270){
-    console.log('Subtract x4')
-      bank = bank -(20000 * 4)
-    console.log(bank)
-      bankText.innerText =bank
+  else if(rando >225 && rando <=270){
+  bank = bank -(20000 * 4)
+  bankText.text(bank)
   }
-      else if(rando >270 && rando <=315){
-    console.log('Add x5')
-        bank = bank + (20000 * 5)
-    console.log(bank)
-        bankText.innerText =bank
+  else if(rando >270 && rando <=315){
+  bank = bank + (20000 * 5)
+  bankText.text(bank)
   }
-      else if(rando >315 && rando <=360){
-    console.log('Subtract x5')
-        bank = bank -(20000 * 5)
-    console.log(bank)
-        bankText.innerText =bank
+  else if(rando >315 && rando <=360){
+  bank = bank -(20000 * 5)
+  bankText.text(bank)
   }
 });
-
-
- 
 
 
